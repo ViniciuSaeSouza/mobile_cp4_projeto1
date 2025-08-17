108991dfec2723eaf5c2b6f792f28c8a72e658b4
ViniciuSaeSouza <viniciusaesouza@gmail.com>
2025-08-17 16:19:54 -0300

    ref: button and title visual changes

--- buscadorMockApi/src/Main.tsx
+++ buscadorMockApi/src/Main.tsx
@@ -7,6 +7,7 @@ import {
   ScrollView,
   StyleSheet,
   Text,
+  TouchableOpacity,
   View,
 } from "react-native";
 import { StatusBar } from "expo-status-bar";
@@ -40,27 +41,31 @@ export default function Main() {
     );
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-      <Text style={styles.titleText}>Dados dos usuários</Text>
-      {usuarios?.map((usuario: Usuario) => (
-        <View style={styles.userContainer} key={usuario.id}>
-          <View style={styles.userCard}>
-            <Field label="Nome" value={usuario.name} />
-            <Field label="Email" value={usuario.email} />
-            <Field label="Cidade" value={usuario.address.city} />
+      <View style={styles.topContainer}>
+        <Text style={styles.titleText}>Dados dos usuários</Text>
+      </View>
+      <ScrollView style={styles.scrollContainer}>
+        {usuarios?.map((usuario: Usuario) => (
+          <View style={styles.userContainer} key={usuario.id}>
+            <View style={styles.userCard}>
+              <Field label="Nome" value={usuario.name} />
+              <Field label="Email" value={usuario.email} />
+              <Field label="Cidade" value={usuario.address.city} />
+            </View>
           </View>
-        </View>
-      ))}
-      <Button
-        title="🔃Refetch"
-        onPress={() => {
-          //   console.log("Refetch button pressed");
-          //   queryClient.invalidateQueries({ queryKey: ["users"] });
-          refetch();
-        }}
-      ></Button>
-    </View>
+        ))}
+      </ScrollView>
+      <View style={styles.bottomContainer}>
+        <TouchableOpacity
+          style={styles.refetchButton}
+          onPress={() => refetch()}
+        >
+          <Text style={styles.refetchButtonColor}>Refetch</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -70,22 +75,37 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    marginTop: "5%",
+  },
+  topContainer: {
+    backgroundColor: "#2968bbff",
+    width: "100%",
   },
   titleText: {
     fontSize: 24,
     fontWeight: "bold",
-    marginBottom: 14,
+    padding: 8,
+    textAlign: "center",
+    color: "#fff",
+  },
+  scrollContainer: {
+    paddingVertical: 10,
+    width: "90%",
+    margin: "auto",
   },
   userContainer: {
     alignSelf: "flex-start",
+    width: "100%",
   },
   userCard: {
     textAlign: "left",
-    marginLeft: 18,
-    paddingLeft: 10,
-    marginBottom: 14,
+    paddingLeft: 8,
+    paddingVertical: 10,
+    marginBottom: 0,
     borderLeftWidth: 1,
     borderBottomWidth: 2,
+    borderTopWidth: 2,
+    width: "100%",
   },
   fetchIndicator: {
     textAlign: "center",
@@ -95,5 +115,23 @@ const styles = StyleSheet.create({
   fetchIndicatorText: {
     fontSize: 28,
     fontWeight: "bold",
+  },
+  bottomContainer: {
+    padding: 12,
+    width: "100%",
+    backgroundColor: "#302f2fff",
+    alignItems: "center",
+  },
+  refetchButton: {
+    backgroundColor: "#388effff",
+    borderRadius: 5,
+    padding: 10,
+    minWidth: "50%",
+  },
+  refetchButtonColor: {
+    textAlign: "center",
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 18,
   },
 });
